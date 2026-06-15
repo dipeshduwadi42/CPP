92238b74a1343408c005d1018bf7bbf9a23b519f
dipeshduwadi42 <dipeshduwadi42@gmail.com>
2026-06-15 19:29:18 +0545

    Add files via upload

---
+++ CPP/tocheckperfectnumber.cpp
@@ -0,0 +1,24 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	int num,i,sum=0;
+	cout<<"Enter a number: ";
+	cin>>num;
+	for(i=1;i<num;i++)
+	{
+		if(num%i==0)
+		{
+			sum=sum+i;
+		}
+	}
+	if(sum==num)
+	{
+		cout<<num<<" is a perfect number";
+	}
+	else
+	{
+		cout<<num<<" is not a perfect number";
+	}
+	return 0;
+}
