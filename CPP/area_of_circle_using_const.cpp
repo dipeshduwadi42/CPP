36db0139672d5cc9c577a6e676333133b0449a8c
dipeshduwadi42 <dipeshduwadi42@gmail.com>
2026-06-19 17:30:09 +0545

    Add files via upload

---
+++ CPP/area_of_circle_using_const.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+	const float pi=3.14;
+	float radius;
+	float area;
+	cout<<"Enter the radius";
+	cin>>radius;
+	area=pi*radius*radius;
+	cout<<"The area of circle is "<<area<<endl;
+}
